ae37a036849fa936d184a2b4d4d24534bb26e685
Meganova301 <XBXSTH003@myuct.ac.za>
2026-03-06 14:16:51 +0200

    Enhance user prompts and login handling
    Updated user prompts for signup and login processes to include an option to return to the main menu. Modified the logged_in function to accept a parameter indicating whether the user should log in.

--- main/server.py
+++ main/server.py
@@ -13,7 +13,7 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(ADDRESS)
 
 OPTIONS1 = "PLEASE PRESS A NUMBER TO CONTINUE:\n1.SIGN-UP\n2.LOGIN\n3.DISCONNECT\nnote:OTHER OPTIONS COMMING?\n"
-OPTIONS2 = "PLEASE PRESS A NUMBER TO CONTINUE:\n1.P2P CONNECTION\n2.CREATE GROUP\n3.JOIN GROUP\n4.CHANGE PASSWORD\n5.CHANGE USERNAME\n6.RETURN TO HOME PAGE\n7.DISCONNECT" 
+OPTIONS2 = "PLEASE PRESS A NUMBER TO CONTINUE:\n1.P2P CONNECTION\n2.CREATE GROUP\n3.JOIN GROUP\n4.CHANGE PASSWORD\n5.CHANGE USERNAME\n6.RETURN TO HOME PAGE\n7.DISCONNECT\n" 
 INV_INPUT = "INVALID INPUT!! PLEASE TRY AGAIN"
 INV_USER = "THE USER_ID IS INVALID, please try again"
 USERNAME_USED = "THIS USERNAME IS ALREADY IN USE, TRY ANOTHER ONE"
@@ -43,17 +43,23 @@ def receive_message(connection):
 def signup(connection):
     signing_up = True
     while signing_up:
-        send_message(connection, "PLEASE ENTER USER ID: ")
+        send_message(connection, "PLEASE ENTER USER ID(or q to return to options): ")
         user_id = receive_message(connection)
         if user_id:
+            if user_id == "q":
+                signing_up = False
+                continue
             if user_id in user_dict:
                 send_message(connection, USERNAME_USED)
                 continue
             send_message(connection, USERNAME_ACCEPT)
             print(f"your user id was: {user_id}")
-            send_message(connection, "PLEASE ENTER YOU PASSWORD: ")
+            send_message(connection, "PLEASE ENTER YOU PASSWORD(or q to return to options): ")
             user_password = receive_message(connection)
             if user_password:
+                if user_password == "q":
+                    signing_up = False
+                    continue
                 print(f"your password was: {user_password}")
                 user_dict[user_id] = user_password
                 send_message(connection, "SIGNED-UP SUCCESSFULLY!!")
@@ -61,23 +67,34 @@ def signup(connection):
 
 def login(connection):
     logging_in = True
+    should_log_in = True #to decide whether to proceed with the "logged in options"
     while logging_in:
         entering_user_name = True
         while entering_user_name:
-            send_message(connection, "PLEASE ENTER USER ID: ")
+            send_message(connection, "PLEASE ENTER USER ID(or q to return to options): ")
             user_id = receive_message(connection)
             if user_id:
+                if user_id == "q":
+                    entering_user_name = False
+                    logging_in = False
+                    should_log_in = False
+                    continue
                 if not user_id in user_dict:
                     send_message(connection, INV_USER)
                     continue
                 send_message(connection, USERNAME_ACCEPT)
                 entering_user_name = False
-
+        if logging_in == False:
+            continue
         entering_password = True
         while entering_password:
-            send_message(connection, "PLEASE ENTER YOU PASSWORD: ")
+            send_message(connection, "PLEASE ENTER YOU PASSWORD(or q to return to options): ")
             user_password = receive_message(connection)
             if user_password:
+                if user_password == "q":
+                    entering_password = False
+                    should_log_in = False
+                    continue
                 if user_dict[user_id] == user_password:
                     send_message(connection, PASSWORD_ACCEPT)
                     send_message(connection, "LOGGED-IN SUCCESSFULLY!!")
@@ -86,9 +103,11 @@ def login(connection):
                 else:
                     send_message(connection, PASSWORD_WRONG)
         logging_in = False
+    return should_log_in
 
-def logged_in(connection):
-    logged_in = True
+def logged_in(connection, should_login):
+    logged_in = should_login
+    still_connected = True # after method execution over, am i still connected to the server
 #OPTIONS2 = "PLEASE PRESS A NUMBER TO CONTINUE:\n1.P2P CONNECTION\n2.CREATE GROUP\n3.JOIN GROUP\n4.CHANGE PASSWORD\n5.CHANGE USERNAME\n6.RETURN TO HOME PAGE\n7.DISCONNECT" 
     while logged_in:
         send_message(connection, OPTIONS2)
@@ -109,13 +128,17 @@ def logged_in(connection):
             # Sthembiso: change username
             pass
         elif choice == "6":
-            return True
+            logged_in = False
+            still_connected = True
+            continue
         elif choice == "7":
             logged_in = False
+            still_connected = False
         else:
             send_message(connection, INV_USER)
-    return False
-    
+    return still_connected
+
+
 def thread_manage(connection, address):
     print(f"{address} CONNECTED SUCCESSFULLY!!")
     is_connected = True
@@ -128,8 +151,10 @@ def thread_manage(connection, address):
                 signup(connection)
                 continue
             elif msg == "2":
-                login(connection)
-                is_connected = logged_in(connection)
+                should_login = login(connection)
+                if should_login == False:
+                    continue
+                is_connected = logged_in(connection, should_login)
                 if is_connected == False:
                     print(f"{address} {DISCONNECT_MESSAGE}")
                     send_message(connection, DISCONNECT_MESSAGE)
@@ -149,5 +174,6 @@ def begin():
         connection, address = server_socket.accept()
         thread = threading.Thread(target=thread_manage, args=(connection, address))
         thread.start()
+
 print("the socket server is running...")
 begin()
